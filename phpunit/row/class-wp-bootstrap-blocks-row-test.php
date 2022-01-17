8133eaf80a96e4c485aa5aa3df60487c60f240f8
Milan Bombsch <milan.bombsch@liip.ch>
2022-01-17 17:16:36 +0100

    add test for row anchor

--- phpunit/row/class-wp-bootstrap-blocks-row-test.php
+++ phpunit/row/class-wp-bootstrap-blocks-row-test.php
@@ -70,4 +70,13 @@ class WP_Bootstrap_Blocks_Row_Test extends WP_Bootstrap_Blocks_UnitTestCase {
 		list( $expected, $actual ) = $this->get_block_output( $variant );
 		$this->assertEquals( $expected, $actual );
 	}
+
+	/**
+	 * Tests row ancher (a.k.a. block id).
+	 */
+	public function test_row_anchor() {
+		$variant = 'anchor';
+		list( $expected, $actual ) = $this->get_block_output( $variant );
+		$this->assertEquals( $expected, $actual );
+	}
 }
